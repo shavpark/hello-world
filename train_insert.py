a56c5ff1f6bd36454313a319facdad1e62c9f168
Shavon <shavpark@iu.edu>
2021-12-13 19:04:50 -0600

    python insert

---
+++ train_insert.py
@@ -0,0 +1,43 @@
+"""import itinerary data 
+with Pandas instead of SQL
+Data source: 
+    passengers.csv in subfolder `train_db`
+Requirements:
+    user-defined module `config.py` in subfolder `function`
+"""
+import sqlalchemy
+import sys, os
+import pymysql
+
+# import config from subdirectory `function`
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from function import config
+
+import pandas as pd
+#pip install mysql-connector-python
+#pip install pymysql
+#pip install sqlalchemy
+
+input_file = os.path.join("train_db", "Amenities.csv")
+df = pd.read_csv(input_file)
+# insert TicketOfficeID to df
+n_row = df.shape[0]
+#df.insert(0, "TicketOfficeID", range(1, n_row+1))
+#print(df[0:2])
+
+# make sqlalchemy.engine
+db = config.sqlalchemy_mysql("train_db2")
+
+# transaction
+if db is not None:
+    #insert, three modes:fail,replace,append. index to False, skip row index
+    df.to_sql('Amenities', con=db, if_exists='append', index=False) 
+    #airlines_df.to_csv(f, index=False, line_terminator='\n')
+    test = pd.read_sql("select * from Amenities", db)
+    print(test[0:5])
+    #close connection
+    #db.close()  
+else:
+    print(f"No connection available.")
